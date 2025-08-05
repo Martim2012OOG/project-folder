580f6ab04f4ba36673f9b4a53f646b7f288ed0c7
Martim2012OOG <nobrega.martimrebelo1@gmail.com>
2025-08-05 14:38:20 +0100

    Update keyManager.js

--- utils/keyManager.js
+++ utils/keyManager.js
@@ -1,1 +1,15 @@
+const keys = {};
 
+export function generateKey(ip) {
+  const key = `${Math.random().toString(36).substring(2, 10)}-${Date.now()}`;
+  keys[ip] = { key, time: Date.now() };
+  return key;
+}
+
+export function isValidKey(k, ip) {
+  const data = keys[ip];
+  if (!data) return false;
+  if (data.key !== k) return false;
+  if (Date.now() - data.time > 86400000) return false; // 24h
+  return true;
+}
